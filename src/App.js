9603d9500fdeaa1f71ac63b0f0c266486c639273
Luke Billings <lukecbillings@gmail.com>
2024-08-14 16:17:43 +0100

    set x when click square

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function Square( { value }) {
-  return <button className="square">{value}</button>
+import { useState } from "react";
+
+
+function Square() {
+  const [value, setValue ] = useState(null);     
+  function handleCLick() {
+    setValue('X');
+  }
+
+  return (
+    <button className="square"
+    onClick={handleCLick}>
+    {value}
+    </button>
+  );
 }
 
 
@@ -7,19 +20,19 @@ export default function Board() {
   return (
     <>
     <div className="board-row">
-      <Square value="1"/>
-      <Square value="2"/>
-      <Square value="3"/>
+      <Square/>
+      <Square/>
+      <Square/>
     </div>
     <div className="board-row">
-      <Square value="4"/>
-      <Square value="5"/>
-      <Square value="6"/>
+      <Square/>
+      <Square/>
+      <Square/>
       </div>
     <div className="board-row">
-      <Square value="7"/>
-      <Square value="8"/>
-      <Square value="9"/>
+      <Square/>
+      <Square/>
+      <Square/>
     </div>
     </>
   )
